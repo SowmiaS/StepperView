d87f96f18578674256bd2dc5c9b4fd33968313cc
Sowmia <sowmiasundar@gmail.com>
2021-11-29 22:18:04 +0530

    added Repository with sample data for stepperview

---
+++ app/src/main/java/com/ss/stepperview/sample/data/Repository.kt
@@ -0,0 +1,14 @@
+package com.ss.stepperview.sample.data
+
+val steps = listOf(
+    "Create WooCommerce website",
+    "Install Jetpack plugin",
+    "Install Woocommerce plugin",
+    "Activate Woocommerce plugin",
+    "Activate Jetpack plugin",
+    "Install app",
+    "Code",
+    "Write test",
+    "Test the app",
+    "Congrats!!"
+)
