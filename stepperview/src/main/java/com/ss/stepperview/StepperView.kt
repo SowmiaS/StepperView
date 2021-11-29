05aa46d9d4f0c5303b2da358c0b1716e2b4735d8
Sowmia <sowmiasundar@gmail.com>
2021-11-29 22:20:47 +0530

    added stepperviewlayout to displaydata one below another

--- stepperview/src/main/java/com/ss/stepperview/StepperView.kt
+++ stepperview/src/main/java/com/ss/stepperview/StepperView.kt
@@ -1,8 +1,40 @@
 package com.ss.stepperview
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
 
 @Composable
-fun StepperView(content: @Composable () -> Unit){
+fun StepperView(
+    items: List<Any>,
+    content: @Composable () -> Unit
+) {
+    StepperViewLayout {
+        content()
+    }
+}
+
+@Composable
+fun Step(
+    content: @Composable () -> Unit
+) {
+    content()
+}
+
+@Composable
+fun StepperViewLayout(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(content = { content() }) { measurables, constraints ->
+        val placeables = measurables.map { it.measure(constraints = constraints) }
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            var y = 0
+            placeables.forEach {
+                it.placeRelative(x = 0, y = y)
+                y += it.height
+            }
+        }
+    }
 
 }
