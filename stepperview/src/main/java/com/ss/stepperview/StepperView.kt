cac8dea94e41dd04f5f749b564e0a5012f6b80cb
Sowmia <sowmiasundar@gmail.com>
2021-11-29 22:38:42 +0530

    added simple stepperviewindicator and stepperviewline to content

--- stepperview/src/main/java/com/ss/stepperview/StepperView.kt
+++ stepperview/src/main/java/com/ss/stepperview/StepperView.kt
@@ -1,8 +1,19 @@
 package com.ss.stepperview
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun StepperView(
@@ -10,6 +21,12 @@ fun StepperView(
     content: @Composable () -> Unit
 ) {
     StepperViewLayout {
+        repeat(items.size - 1) {
+            StepperViewIndicator()
+        }
+        repeat(items.size - 1) {
+            StepperViewLine()
+        }
         content()
     }
 }
@@ -36,5 +53,28 @@ fun StepperViewLayout(
             }
         }
     }
+}
 
+@Composable
+fun StepperViewIndicator() {
+    OutlinedButton(
+        onClick = { /*TODO*/ },
+        modifier = Modifier.size(50.dp),
+        shape = CircleShape,
+        border = BorderStroke(1.dp, Color.Blue),
+        contentPadding = PaddingValues(0.dp),
+        colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Blue)
+    ) {
+        Icon(Icons.Default.Add, contentDescription = "StepperViewIndicator")
+    }
+}
+
+@Composable
+fun StepperViewLine() {
+    Box(
+        modifier = Modifier
+            .height(50.dp)
+            .width(10.dp)
+            .background(Color.Blue)
+    )
 }
