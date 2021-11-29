f88bbb29c106fb3ecfb92f51f8d96951ff27d170
Sowmia <sowmiasundar@gmail.com>
2021-11-30 02:28:04 +0530

    updated sample stepperview with different sized steps

--- app/src/main/java/com/ss/stepperview/sample/MainActivity.kt
+++ app/src/main/java/com/ss/stepperview/sample/MainActivity.kt
@@ -3,11 +3,17 @@ package com.ss.stepperview.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ss.stepperview.Step
 import com.ss.stepperview.StepperView
 import com.ss.stepperview.sample.data.steps
@@ -16,14 +22,20 @@ import com.ss.stepperview.sample.ui.theme.StepperViewTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // for testing different sized steps
+        val heights = arrayOf( 50.dp , 100.dp , 40.dp, 30.dp, 20.dp )
         setContent {
             StepperViewTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     StepperView(items = steps) {
-                        steps.forEach {
+                        steps.forEachIndexed { index, it ->
                             Step {
-                                Text(text = it)
+                                Text(
+                                    modifier = Modifier.height(heights[index]).background(Color.Red),
+                                    textAlign = TextAlign.Center,
+                                    text = it
+                                )
                             }
 
                         }
