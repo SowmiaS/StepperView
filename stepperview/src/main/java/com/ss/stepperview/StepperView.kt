a4d2cb714e314d064db7e309a10d62d0b2c55c70
Sowmia <sowmiasundar@gmail.com>
2021-12-20 15:37:54 +0530

    implementation of step alignments and no of steps per row

--- stepperview/src/main/java/com/ss/stepperview/StepperView.kt
+++ stepperview/src/main/java/com/ss/stepperview/StepperView.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.IconButton
 import androidx.compose.material.OutlinedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -22,20 +23,41 @@ import kotlin.math.max
 private const val LAYOUTID_STEPPERVIEW_INDICATOR = "LAYOUTID_STEPPERVIEW_INDICATOR"
 private const val LAYOUTID_STEPPERVIEW_LINE = "LAYOUTID_STEPPERVIEW_LINE"
 
+enum class StepsPerRow(val noOfSteps: Int){
+    ONE(1), TWO(2)
+}
 @Composable
 fun StepperView(
     items: List<Any>,
-    content: @Composable () -> Unit
+    stepsPerRow : StepsPerRow = StepsPerRow.ONE,
+    content: @Composable StepScope.() -> Unit
 ) {
-    StepperViewLayout {
-        repeat(items.size) {
-            StepperViewIndicator(modifier = Modifier.layoutId(LAYOUTID_STEPPERVIEW_INDICATOR.plus(it)))
-        }
-        repeat(items.size - 1) {
-            StepperViewLine(modifier = Modifier.layoutId(LAYOUTID_STEPPERVIEW_LINE.plus(it)))
-        }
-        content()
+
+    IconButton(onClick = { /*TODO*/ }) {
+
     }
+    StepperViewLayout(
+        Modifier,
+        stepsPerRow  = stepsPerRow,
+        indicatorContent = {
+            repeat(items.size) {
+                StepperViewIndicator(
+                    modifier = Modifier.layoutId(
+                        LAYOUTID_STEPPERVIEW_INDICATOR.plus(it)
+                    )
+                )
+            }
+        },
+        lineContent = {
+            repeat(items.size - 1) {
+                StepperViewLine(modifier = Modifier.layoutId(LAYOUTID_STEPPERVIEW_LINE.plus(it)))
+            }
+        },
+        stepContent = {
+            StepScopeInstance.content()
+        }
+    )
+
 }
 
 @Composable
@@ -46,28 +68,55 @@ fun Step(
 }
 
 private val Measurable.stepAlignment
-        get() = (parentData as? StepAlignmentData)?.stepAlignment ?: StepAlignment.RIGHT
+    get() = (parentData as? StepAlignmentData)?.stepAlignment ?: StepAlignment.RIGHT
 
 
 @Composable
 fun StepperViewLayout(
     modifier: Modifier = Modifier,
-    content: @Composable () -> Unit
+    stepsPerRow : StepsPerRow,
+    indicatorContent: @Composable () -> Unit,
+    lineContent: @Composable () -> Unit,
+    stepContent: @Composable StepScope.() -> Unit
 ) {
     Layout(
-        content = { content() },
+        content = {
+            indicatorContent()
+            lineContent()
+            StepScopeInstance.stepContent()
+        },
         modifier = modifier
     ) { measurables, constraints ->
 
         val horizontalSpacing = 50 // Spacing between Indicator and Step.
         var verticalSpacing = 100 // Spacing between Steps.
 
-        val stepsPlaceables = measurables
+        val stepsMeasurables = measurables
             .filter {
                 it.layoutId.toString().contains(LAYOUTID_STEPPERVIEW_LINE)
-                    .not() && it.layoutId.toString().contains(LAYOUTID_STEPPERVIEW_INDICATOR).not()
+                    .not() && it.layoutId.toString().contains(LAYOUTID_STEPPERVIEW_INDICATOR)
+                    .not()
             }
-            .map { it.measure(constraints = constraints) }
+        val alignments = ArrayList<StepAlignment>()
+
+        var maxLeftStepWidth = 0
+        var maxRightStepWidth = 0
+        var indicatorXPosition = 0
+        val stepsPlaceables = stepsMeasurables
+            .map {
+              val  placeable = it.measure(constraints = constraints)
+              alignments.add(it.stepAlignment)
+              if(it.stepAlignment == StepAlignment.LEFT) {
+                  maxLeftStepWidth = Math.max(maxLeftStepWidth, placeable.width)
+              }else{
+                  maxRightStepWidth = Math.max(maxRightStepWidth, placeable.width)
+              }
+              placeable
+            }
+        if(maxLeftStepWidth != 0){
+            indicatorXPosition = maxLeftStepWidth + horizontalSpacing
+        }
+
 
         val stepIndicatorPlaceables = measurables
             .filter { it.layoutId.toString().contains(LAYOUTID_STEPPERVIEW_INDICATOR) }
@@ -77,7 +126,7 @@ fun StepperViewLayout(
             .filter { it.layoutId.toString().contains(LAYOUTID_STEPPERVIEW_LINE) }
             .mapIndexed { index, measurable ->
                 val lineHeight =
-                    (stepsPlaceables[index].height / 2 + stepsPlaceables[index + 1].height / 2).plus(
+                    (stepsPlaceables[index/stepsPerRow.noOfSteps].height / 2 + stepsPlaceables[(index/stepsPerRow.noOfSteps) + stepsPerRow.noOfSteps].height / 2).plus(
                         verticalSpacing
                     ).minus(stepIndicatorPlaceables[index].height)
                 measurable.measure(
@@ -88,6 +137,12 @@ fun StepperViewLayout(
                 )
             }
 
+        val maxWidthIndicatorAndLine =
+            stepIndicatorPlaceables.plus(stepLinePlaceables).maxOf { it.width }
+
+        val leftStepXPosition = 0
+        val rightStepXPosition = maxLeftStepWidth + maxWidthIndicatorAndLine + horizontalSpacing
+
         layout(constraints.maxWidth, constraints.maxHeight) {
             val noOfSteps = stepsPlaceables.size
             var y = 0
@@ -97,32 +152,55 @@ fun StepperViewLayout(
                 y = (stepIndicatorPlaceables[0].height / 2) - (stepsPlaceables[0].height / 2)
                 verticalSpacing += stepIndicatorPlaceables[0].height
             }
-            for (i in 0 until noOfSteps) {
+            for (i in 0 until noOfSteps step stepsPerRow.noOfSteps) {
                 val stepPlaceable = stepsPlaceables[i]
-                val indicatorPlaceable = stepIndicatorPlaceables[i]
+                val indicatorPlaceable = stepIndicatorPlaceables[i/stepsPerRow.noOfSteps]
                 var linePlaceable: Placeable? = null
                 var linePlaceableWidth = 0
 
                 // no of lines is one less than the no of steps.
-                if (i <= stepLinePlaceables.size - 1) {
-                    linePlaceable = stepLinePlaceables[i]
+                if ((i/stepsPerRow.noOfSteps) <= stepLinePlaceables.size - 1) {
+                    linePlaceable = stepLinePlaceables[i/stepsPerRow.noOfSteps]
                     linePlaceableWidth = linePlaceable.width
                 }
 
+
                 // Placing Steps
-                val x = max(indicatorPlaceable.width, linePlaceableWidth).plus(horizontalSpacing)
-                stepPlaceable.placeRelative(x = x, y = y)
+                for( j in i until i+stepsPerRow.noOfSteps) {
+
+                    var x =
+                        max(indicatorPlaceable.width, linePlaceableWidth).plus(horizontalSpacing)
+                    when (alignments[j]) {
+                        StepAlignment.RIGHT -> x = rightStepXPosition
+                        StepAlignment.LEFT -> x = leftStepXPosition
+                    }
+                    stepsPlaceables[j].placeRelative(x = x, y = y)
+                }
+
                 y += stepPlaceable.height
 
                 // Placing Indicators
-                val indicatorY =
-                    (y - (stepPlaceable.height / 2)) - (indicatorPlaceable.height / 2)   // depends on how indicator has to be shown centre, top, bottom . For now center
-                indicatorPlaceable.placeRelative(x = 0, y = indicatorY)
-                val indicatorXMid = indicatorPlaceable.width / 2
+                val indicatorAlignment: StepIndicatorAlignment = StepIndicatorAlignment.CENTER
+                var indicatorY = 0
+
+                when (indicatorAlignment) {
+                    StepIndicatorAlignment.CENTER -> indicatorY =
+                        (y - (stepPlaceable.height / 2)) - (indicatorPlaceable.height / 2)   // depends on how indicator has to be shown centre, top, bottom . For now center
+                    StepIndicatorAlignment.TOP -> indicatorY =
+                        y - (indicatorPlaceable.height / 2)   // depends on how indicator has to be shown centre, top, bottom . For now center
+                    StepIndicatorAlignment.BOTTOM -> indicatorY =
+                        y - (stepPlaceable.height - (indicatorPlaceable.height / 2))  // depends on how indicator has to be shown centre, top, bottom . For now center
+                }
+
+                indicatorPlaceable.placeRelative(x = indicatorXPosition , y = indicatorY)
+                val indicatorXMid = indicatorXPosition + indicatorPlaceable.width / 2
 
                 // Placing Line
-                val lineY = (y - (stepPlaceable.height / 2)) + indicatorPlaceable.height / 2
-                linePlaceable?.placeRelative(x = indicatorXMid - linePlaceableWidth / 2, y = lineY)
+                val lineY = indicatorY + indicatorPlaceable.height
+                linePlaceable?.placeRelative(
+                    x = indicatorXMid - linePlaceableWidth / 2,
+                    y = lineY
+                )
 
                 // Adding vertical spacing
                 y += verticalSpacing
@@ -167,12 +245,14 @@ class StepAlignmentData(val stepAlignment: StepAlignment) : ParentDataModifier {
 }
 
 interface StepScope {
+    fun Modifier.align(stepAlignment: StepAlignment) : Modifier
+}
 
-    fun Modifier.align(stepAlignment: StepAlignment) {
+internal object StepScopeInstance : StepScope {
+
+    override fun Modifier.align(stepAlignment: StepAlignment) : Modifier = this.then(
         StepAlignmentData(stepAlignment)
-    }
-
-    companion object StepScope
+    )
 }
 
 enum class StepIndicatorAlignment {
@@ -181,7 +261,8 @@ enum class StepIndicatorAlignment {
     CENTER
 }
 
-class StepIndicatorAlignmentData(val stepIndicatorAlignment: StepIndicatorAlignment) : ParentDataModifier {
+class StepIndicatorAlignmentData(val stepIndicatorAlignment: StepIndicatorAlignment) :
+    ParentDataModifier {
 
     override fun Density.modifyParentData(parentData: Any?): Any? {
         return this@StepIndicatorAlignmentData
