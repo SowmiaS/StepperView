89cf014babc200712ec1f80d6132ad2ec48648cc
Sowmia <sowmiasundar@gmail.com>
2022-01-09 22:23:53 +0530

    Configuring Detekt static analyser

--- stepperview/src/main/java/com/ss/stepperview/view/StepperViewIndicator.kt
+++ stepperview/src/main/java/com/ss/stepperview/view/StepperViewIndicator.kt
@@ -9,17 +9,25 @@ import androidx.compose.material.OutlinedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun StepperViewIndicator(modifier: Modifier = Modifier) {
+fun StepperViewIndicator(
+    modifier: Modifier,
+    border: BorderStroke = BorderStroke(1.dp, Color.Blue),
+    contentPadding: PaddingValues = PaddingValues(5.dp),
+    size: Dp = 10.dp,
+    color: Color = Color.Yellow
+) {
     OutlinedButton(
         onClick = { /*TODO*/ },
-        modifier = modifier.size(10.dp),
+        modifier = modifier.size(size),
         shape = CircleShape,
-        border = BorderStroke(1.dp, Color.Blue),
-        contentPadding = PaddingValues(0.dp),
-        colors = ButtonDefaults.outlinedButtonColors(contentColor = Color.Blue)
+        border = border,
+        contentPadding = contentPadding,
+        colors = ButtonDefaults.outlinedButtonColors(backgroundColor = color)
     ) {
         //Icon(Icons.Default.Add, contentDescription = "StepperViewIndicator")
     }
